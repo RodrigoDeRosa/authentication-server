3d1feef56afab836251caac3ff5c663e94b11783
Rodrigo De Rosa <rodrigoderosa15@gmail.com>
2022-02-20 18:30:07 +0100

    Added configuration tests;

---
+++ test/utils/configuration/configuration_test.py
@@ -0,0 +1,30 @@
+from os.path import abspath, join, dirname
+from unittest import TestCase
+
+from app.model.errors.invalid_configuration_key_error import InvalidConfigurationKeyError
+from app.utils.configuration.configuration import Configuration
+
+
+class ConfigurationTest(TestCase):
+
+    def setUp(self) -> None:
+        Configuration.ENV_CONF_FOLDER_PATH = \
+            f'{abspath(join(dirname(__file__), "../../"))}/resources/config/env/'
+        Configuration.SENSITIVE_CONF_PATH = \
+            f'{abspath(join(dirname(__file__), "../../"))}/resources/config/sensitive.conf'
+        self.config = Configuration(env='test')
+
+    def test_read_value(self):
+        self.assertEqual('test_db', self.config.get('database.name'))
+
+    def test_read_with_default(self):
+        self.assertEqual('Hello World!', self.config.get('invalidKey', 'Hello World!'))
+
+    def test_read_value_not_present(self):
+        with self.assertRaises(InvalidConfigurationKeyError) as context:
+            self.config.get('invalid_key')
+        self.assertTrue('Failed to read' in context.exception.message)
+
+    def test_sensitive_conf(self):
+        config = Configuration(env='test')
+        self.assertEqual('fakeSecret', config.get('app.secret'))
